01e3a989ecdd30a2d780af418727746bceb4a02e
Focbri <joanfpg2002@gmail.com>
2024-11-15 17:12:06 -0500

    molde de productos y optimizacion de prod. liquidacion

---
+++ src/data/productosData.ts
@@ -0,0 +1,20 @@
+import aioLenovo from "../assets/PLiquidacion/aioLenovo.png";
+
+
+export const swCisco = [
+  {
+    imgSrc: aioLenovo.src,
+    title: "Switch Cisco 1",
+    descriptions: ["Alta velocidad", "24 puertos", "1 año de garantía"],
+    precio: "$150",
+    whatsappLink: "https://wa.me/123456789?text=Estoy%20interesado%20en%20el%20producto%201",
+  },
+  {
+    imgSrc: aioLenovo.src,
+    title: "Switch Cisco 2",
+    descriptions: ["Confiable", "8 puertos", "2 años de garantía"],
+    precio: "$80",
+    whatsappLink: "https://wa.me/123456789?text=Estoy%20interesado%20en%20el%20producto%202",
+  },
+  // Agrega más productos si es necesario
+];
